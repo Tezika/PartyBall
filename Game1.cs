b58fc58e3475a242b49c0c0dac967f1e2ebd303a
FENGKAI ZHOU <u1216510@ad.utah.edu>
2018-08-22 18:13:41 -0600

    modify some stuff

--- Game1.cs
+++ Game1.cs
@@ -53,7 +53,7 @@ namespace PartyBall
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
+        /// <summary>    
         /// Allows the game to run logic such as updating the world,
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
