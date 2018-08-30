27f1feb8de840789b3b30d0e5df71887ac0bfa1d
Tezika <zfk565614653@outlook.com>
2018-08-30 17:55:03 -0600

    change the camera which only move vetically.

--- Scripts/Level/Level.cs
+++ Scripts/Level/Level.cs
@@ -46,53 +46,6 @@ namespace PartyBall.Scripts.Level
             this.LayoutWalls(graphicsDevice, 0.25f, layoutHeight);
             this.LayoutPickup(graphicsDevice, 1.0f, layoutHeight);
 
-            //var wallScale = 0.25f;
-            //var regPlatform_1 = new Pipe(Graphics.Reg_Platform, Vector2.Zero, platformScale);
-            //var regPlatform_2 = new Pipe(Graphics.Reg_Platform, Vector2.Zero, platformScale);
-            //var wall_1 = new Wall(Graphics.Wall_Left, Vector2.Zero, WallSide.Left, wallScale);
-            //var wall_2 = new Wall(Graphics.Wall_Right, Vector2.Zero, WallSide.Right, wallScale);
-            //var wall_3 = new Wall(Graphics.Wall_Left, Vector2.Zero, WallSide.Left, wallScale);
-            //var wall_4 = new Wall(Graphics.Wall_Right, Vector2.Zero, WallSide.Right, wallScale);
-            //var testPickup = new TestPickUp(Graphics.ShadesPickup, Vector2.Zero);
-
-            //var layoutHeight = regPlatform_1.Height * regPlatform_2.Scale;
-
-            //regPlatform_1.Position = new Vector2((float)(graphicsDevice.Viewport.Width * 0.5),
-            //                                     (float)(graphicsDevice.Viewport.Height - 0.5 * layoutHeight));
-
-            ////wall_1.Position = new Vector2((float)(graphicsDevice.Viewport.Width * 0.2),
-            ////                                   (float)(graphicsDevice.Viewport.Height - 1.7 * layoutHeight));
-
-            ////wall_2.Position = new Vector2((float)(graphicsDevice.Viewport.Width),
-            //                                 //(float)(graphicsDevice.Viewport.Height - 1.7 * layoutHeight));
-
-
-            //regPlatform_2.Position = new Vector2((float)(graphicsDevice.Viewport.Width * 0.5),
-            //                                           (float)(graphicsDevice.Viewport.Height - 1.5 * layoutHeight));
-
-
-            ////wall_3.Position = new Vector2((float)(graphicsDevice.Viewport.Width * 0.2),
-            ////                                   (float)(graphicsDevice.Viewport.Height - 4 * layoutHeight));
-
-            ////wall_4.Position = new Vector2((float)(graphicsDevice.Viewport.Width),
-            //                                 //(float)(graphicsDevice.Viewport.Height - 4.6 * layoutHeight));
-
-
-            //testPickup.Position = new Vector2((float)(graphicsDevice.Viewport.Width * 0.5),
-            //                                           (float)(graphicsDevice.Viewport.Height - 5.3 * layoutHeight));
-
-            //this.Platforms.Add(regPlatform_1);
-            //this.Platforms.Add(regPlatform_2);
-
-            ////this.Platforms.Add(wall_1);
-            ////this.Platforms.Add(wall_2);
-            ////this.Platforms.Add(wall_3);
-            ////this.Platforms.Add(wall_4);
-
-            //this.Pickups.Add(testPickup);
-
-            //this.GameObjects.Add(this.Character);
-            //this.GameObjects.AddRange(this.Platforms);
             this.GameObjects.Add(this.Character);
             this.GameObjects.AddRange(this.Pickups);
             this.GameObjects.AddRange(this.Platforms);
@@ -132,7 +85,7 @@ namespace PartyBall.Scripts.Level
             {
                 this.GameObjects[i].Update(gameTime);
             }
-            this.Camera.FollowCharacter(this.Character);
+            this.Camera.Update(gameTime ,this.Character);
         }
 
         private void LayoutPipes(GraphicsDevice graphicsDevice, float scale, float layoutHeight)
--- Scripts/Render/Camera.cs
+++ Scripts/Render/Camera.cs
@@ -7,16 +7,18 @@ namespace PartyBall.Scripts.Render
     {
         public Matrix Transform { get; private set; }
 
+        public Vector2 Centre { get; private set; }
+
         public Camera()
         {
             this.Transform = new Matrix();
         }
-
-        public void FollowCharacter(Character character)
+ public void Update(GameTime gameTime, Character character)
         {
-            var offset = Matrix.CreateTranslation((int)(RenderManager.Instance.ScreenWidth * 0.6), (int)(RenderManager.Instance.ScreenHeight * 0.8), 0);
-            var position = Matrix.CreateTranslation(-character.Position.X - character.Width / 2, -character.Position.Y - character.Height / 2, 0);
-            this.Transform = offset * position;
+            this.Centre = new Vector2(0, character.Position.Y + character.Height / 2 - RenderManager.Instance.ScreenHeight * 0.8f);
+            this.Transform = Matrix.CreateScale(new Vector3(1, 1, 0)) 
+                            * Matrix.CreateTranslation(new Vector3(-this.Centre.X, -this.Centre.Y, 0));
+
         }
     }
 }
